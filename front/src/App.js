2c45d0c18b74ea2a405be2cb56faebb23fa33afd
github-marynapoliarush <merienf8118@gmail.com>
2023-04-07 14:14:42 +0300

    first page

--- front/src/App.js
+++ front/src/App.js
@@ -7,9 +7,9 @@ function App() {
 
   return (
     <div className="App">
-      <div className='most-main-container'>
+      <div className='content-container'>
         <header>
-          <a href='#' className='logo'> <img src={process.env.PUBLIC_URL + '/imagesHTML/icons/logo.png'} alt='girl'/> </a>
+          <a href='#' className='logo'> <img src={process.env.PUBLIC_URL + '/imagesHTML/icons/logo.png'} alt='logo'/> </a>
           <nav>
             <ul className='nav-list'>
               <li className='nav-li'><a href='#'>КАТАЛОГ</a></li>
@@ -27,6 +27,43 @@ function App() {
           </ul>
         </header>
       </div>
+      <main>
+        <div className='first-page'>
+          <img src={process.env.PUBLIC_URL + '/imagesHTML/girl.jpg'} className='first-page-img' alt='girl'/>
+          <div className='content-container'>
+            <div className='first-page-text-container'>
+              <img src={process.env.PUBLIC_URL + '/imagesHTML/icons/bg.png'} alt='user'/>
+              <p><span className='first-page-text-green'>ОРЕНДА</span> ФОТО </p>
+              <span>І ВІДЕО</span>
+              <span>ОБЛАДНАННЯ</span>
+            </div>
+          </div>
+        </div>
+        <div className='first-page-row'>
+          <div className='content-container row'>
+              <div className='first-page-row-block'>
+                <img src={process.env.PUBLIC_URL + '/imagesHTML/icons/list.png'} alt='list'/>
+                <p>Найбільший вибір техніки</p>
+              </div>
+              <div className='first-page-row-block'>
+                <img src={process.env.PUBLIC_URL + '/imagesHTML/icons/time.png'} alt='time'/>
+                <p>Швидке оформлення замовлення</p>
+              </div>
+              <div className='first-page-row-block'>
+                <img src={process.env.PUBLIC_URL + '/imagesHTML/icons/placeholder.png'} alt='placeholder'/>
+                <p>Можливість забрати замовлення в пункті видачі</p>
+              </div>
+              <div className='first-page-row-block'>
+                <img src={process.env.PUBLIC_URL + '/imagesHTML/icons/product.png'} alt='product'/>
+                <p>Доставка в будь-яку точку міста</p>
+              </div>
+              <div className='first-page-row-block'>
+                <img src={process.env.PUBLIC_URL + '/imagesHTML/icons/shopping.png'} alt='shopping'/>
+                <p>Оплата картою або готівкою</p>
+              </div>
+            </div>
+          </div>
+      </main>
     </div>
   );
 }
